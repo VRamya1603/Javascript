88f5fe1ceae1f94450d2a0c8559febb48ba7abe7
Ramya Venkatachalam <143085867+VRamya1603@users.noreply.github.com>
2023-10-04 19:11:45 +0530

    Update totalnoofwords.js

--- totalnoofwords.js
+++ totalnoofwords.js
@@ -2,7 +2,7 @@
 
 let para =
   "The break keyword allows programs to leave a loop during the execution of its block";
-words = para.split(" ");
+const words = para.split(" ");
 //console.log(words);
-totalnoofwords = words.length;
+const totalnoofwords = words.length;
 console.log("Total no of words in the paragraph : " + totalnoofwords); 
