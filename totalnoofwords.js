0cc8b3e544ef11395cd4b900a11eadeb85319ac7
Ramya Venkatachalam <143085867+VRamya1603@users.noreply.github.com>
2023-10-04 19:09:24 +0530

    Update and rename totalnoofwords.txt to totalnoofwords.js

---
+++ totalnoofwords.js
@@ -0,0 +1,8 @@
+//  Write a program to find out how many words in the given paragraph? 
+
+let para =
+  "The break keyword allows programs to leave a loop during the execution of its block";
+words = para.split(" ");
+//console.log(words);
+totalnoofwords = words.length;
+console.log("Total no of words in the paragraph : " + totalnoofwords); 
